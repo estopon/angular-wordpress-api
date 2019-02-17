96b209d1b8419006a72412c2349eef8ff8221f7a
estopon <estopon@gmail.com>
2019-02-17 08:58:02 +0100

    Configuración inicial

---
+++ src/app/wordpress.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WordpressService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>('https://public-api.wordpress.com/rest/v1.1/sites/viajesgala747870895.wordpress.com/posts/', {
+      params: {
+        per_page: '6'
+      }
+    });
+  }
+}
