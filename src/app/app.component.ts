9dc63ea40d7ce12735e1c473b9cf3ecb401ba009
alfonso.lendinez <alendinez@halconviajes.com>
2019-06-13 08:21:40 +0200

    Cambio de menú de navegación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+import { WordpressService } from './services/wordpress.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,22 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
 
-  constructor() { }
+  categories$: Observable<any[]>;
+
+  constructor(private wp: WordpressService, private router: Router, private route: ActivatedRoute) {
+    this.categories$ = this.wp.getCategories().map(
+      (res: any) => res.categories
+    );
+  }
+
+  navigate(path) {
+    this.router.navigate(['/', 'categories', path]).then(
+      nav => {
+        window.location.reload();
+      }, err => {
+        console.log(err);
+      }
+    );
+  }
 
 }
